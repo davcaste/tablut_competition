8c4ead0ab2b7f18d1f1599ce290ec03042514e80
Lukewtr <55801206+Lukewtr@users.noreply.github.com>
2019-11-27 22:09:28 +0100

    my_games file added
    this file contains our version of alphabeta_cutoff_search algorithm which implements multithreading speed-up

---
+++ my_games.py
@@ -0,0 +1,56 @@
+inf = float('inf')
+
+
+def alphabeta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None, part=1):
+    """Search game to determine best action; use alpha-beta pruning.
+    This version cuts off search and uses an evaluation function."""
+
+    player = game.to_move(state)
+
+    # Functions used by alphabeta
+    def max_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = -inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state, alpha, beta, depth):
+        if cutoff_test(state, depth):
+            return eval_fn(state)
+        v = inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a),
+                                 alpha, beta, depth + 1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alphabeta_cutoff_search starts here:
+    # The default test cuts off at depth d or at a terminal state
+    cutoff_test = (cutoff_test or
+                   (lambda state, depth: depth > d or
+                                         game.terminal_test(state)))
+    eval_fn = eval_fn or (lambda state: game.utility(state, player))
+    best_score = -inf
+    beta = inf
+    best_action = None
+
+    # two different versions to speed-up the computation
+    if part == 1:
+        action_set = game.actions(state)[:len(game.actions(state))//2]
+    else:
+        action_set = game.actions(state)[len(game.actions(state))//2:]
+
+    for a in action_set:
+        v = min_value(game.result(state, a), best_score, beta, 1)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action, best_score      # BEST_SCORES returned with best_action
